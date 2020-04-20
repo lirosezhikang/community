0f3fe0bf33380cba6e0f3d583c25c222baf9e4cc
unknown <2118656521@qq.com>
2020-04-20 13:36:40 +0800

    add cookies

--- src/main/java/com/silence/community/mapper/UserMapper.java
+++ src/main/java/com/silence/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.silence.community.mapper;
 import com.silence.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -10,5 +12,6 @@ public interface UserMapper {
     @Insert("INSERT INTO user_table (account_id, name, token, gmt_create, gmt_modified) VALUES (#{accountId}, #{name}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insertUser(User user);
 
-
+    @Select("select * from user_table where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
